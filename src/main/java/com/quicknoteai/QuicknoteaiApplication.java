5930896ff7973b6fe9c1fbb517c06edd8e2f76e2
Ritesh Shah <arits.king74@gmail.com>
2025-04-11 23:06:01 -0500

    Initial file stucture for quick note ai backend using Spring

---
+++ src/main/java/com/quicknoteai/QuicknoteaiApplication.java
@@ -0,0 +1,13 @@
+package com.quicknoteai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuicknoteaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuicknoteaiApplication.class, args);
+	}
+
+}
