ed656ab7cfce21ac8654b6f04fba04d49b2e6b95
Ritesh Shah <arits.king74@gmail.com>
2025-04-12 10:56:03 -0500

    Holds all the dependencies obtained from Spring

--- src/main/java/com/quicknoteai/QuickNoteAiApplication.java
+++ src/main/java/com/quicknoteai/QuickNoteAiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QuicknoteaiApplication {
+public class QuickNoteAiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QuicknoteaiApplication.class, args);
+		SpringApplication.run(QuickNoteAiApplication.class, args);
 	}
 
 }
