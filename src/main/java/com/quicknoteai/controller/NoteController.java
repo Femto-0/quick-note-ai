bb1d0957367583285182c574eea9e3cc2279d73d
Femto-0 <arits.king74@gmail.com>
2025-08-08 09:18:52 -0500

    removed the commented out faulty code

--- src/main/java/com/quicknoteai/controller/NoteController.java
+++ src/main/java/com/quicknoteai/controller/NoteController.java
@@ -20,44 +20,6 @@ public class NoteController {
     @Autowired
     private OllamaService ollamaService;
 
-//    @PostMapping
-//    public Note processNote(@RequestBody Note note) {
-//        String summary = null;
-//        String mood = null;
-//
-//        // Try generating the summary
-//        try {
-//            System.out.println("Calling my boy llama for summarization...");
-//            summary = ollamaService.getSummary(note.getContent()).block();
-//            System.out.println("Summary received.");
-//        } catch (HttpClientErrorException.TooManyRequests e) {
-//            try {
-//                Thread.sleep(2000);
-//            } catch (InterruptedException ignored) {}
-//        }
-//
-//        // Try determining the mood
-//        try {
-//            System.out.println("Calling my boy llama to check the vibe...");
-//            mood = ollamaService.determineMood(note.getContent()).block();
-//            System.out.println("Vibe received.");
-//        } catch (HttpClientErrorException.TooManyRequests e) {
-//            try {
-//                Thread.sleep(2000);
-//            } catch (InterruptedException ignored) {}
-//        }
-//
-//        // Set the timestamp
-//        note.setTimeStamp(LocalDateTime.now());
-//
-//        // Set summary and mood if available
-//        note.setSummary(summary != null ? summary : "Summary could not be generated.");
-//        note.setMood(mood != null ? mood : "Vibe couldn't be determined");
-//
-//        return noteRepository.save(note);
-//    }
-
-
     /*
     combining result from summary and the mood and returning a json.
      */
